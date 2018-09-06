163b0e121d744519f00b97021ea347856aabc7a2
Jeffrey Taylor <jntaylor001@gmail.com>
2018-09-05 21:08:42 -0500

    First draft of tweet class

--- Sprint1/Sprint1/tweet.cpp
+++ Sprint1/Sprint1/tweet.cpp
@@ -1,6 +1,35 @@
 #include "tweet.h"
+#include <iostream>
+#include <string>
 
-Tweet::Tweet()
-{
+using namespace std;
 
+Tweet::Tweet(long x, string n, string t, int i)
+    :id(x), user(n), text(t), target(i) {
 }
+long Tweet::getID() {
+    return id;
+}
+void Tweet::setID(long x) {
+    id = x;
+}
+string Tweet::getUser() {
+    return user;
+}
+void Tweet::setUser(string a) {
+    user = a;
+}
+string Tweet::getText() {
+    return text;
+}
+void Tweet::setText(string a) {
+    text = a;
+}
+int Tweet::getTarget() {
+    return target;
+}
+void Tweet::setTarget(int x) {
+    target = x;
+}
+Tweet::~Tweet() {}
+
--- Sprint1/Sprint1/tweet.h
+++ Sprint1/Sprint1/tweet.h
@@ -2,14 +2,18 @@
 #define TWEET_H
 #include <string>
 
-class Tweet
-{
+//Make class specifically for tweets.  Will make counting and sorting easier.
+class Tweet {
 public:
-    Tweet(long id, std::string user, std::string text, int target);
+    Tweet(long x, std::string n, std::string t, int i);
     ~Tweet();
 
+    //ID is set to be of type long due to the pattern of 10 digits.
+    //In theory an unsigned int could work, but based on the nature
+    //of the internet, that number could be overtaken and forcibly
+    //reset fairly quickly.
     void setID(long x);
-    int getID();
+    long getID();
 
     void setUser(std::string a);
     std::string getUser();
