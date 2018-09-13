b3d8575d6c91ba29ce5fb657728bf22587e1da02
Jeffrey Taylor <jntaylor001@gmail.com>
2018-09-13 16:55:24 -0500

    The ones that fell between cracks

--- Sprint2/Sprint2/vect.h
+++ Sprint2/Sprint2/vect.h
@@ -1,4 +1,36 @@
 #ifndef VECT_H
 #define VECT_H
 
+//inc
+
+template <typename t>
+
+
+// Header Part
+class Vect {
+private:
+    int size;
+    int capacity;
+    t* data;
+
+
+public:
+    Vect(const Vect<t>&);
+    Vect(int initSize = 10);
+    Vect& operator=(const Vect ct);
+    ~Vect();
+
+    t operator[](int location);
+    t elementAt(int location);
+    void setElementAt(int location, t);
+
+    void push(t);
+    void pop();
+    void popAt(int location);
+};
+
+
+
+
+
 #endif // VECT_H
