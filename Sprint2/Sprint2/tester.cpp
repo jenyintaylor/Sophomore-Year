4c0e859c306a595c4005e001b976e2065ac74c38
Jeffrey Taylor <jntaylor001@gmail.com>
2018-09-21 11:38:49 -0500

    Fixed the TDD.

--- Sprint2/Sprint2/tester.cpp
+++ Sprint2/Sprint2/tester.cpp
@@ -1,22 +1,28 @@
-
-#include "catch.hpp"
 #include "vect.h"
+#include "catch.hpp"
 
 Vect<int> leedle;
 
 void create() {
-
     for(int i = 0; i < 5; i++) {
         int j =(i*10)+5;
         leedle.push(j);
     }
 }
 
-TEST_CASE("Vector has a beginning") {
-    REQUIRE(leedle.first() == 5);
-    REQUIRE(leedle[0] == 0);
-    REQUIRE(leedle.elementAt(0) == 5);
-}
-TEST_CASE("Vector has an end") {
-    REQUIRE(leedle[4] == 45);
+TEST_CASE("Custom Vector Cases") {
+
+    create();
+
+    SECTION("Vector has a beginning") {
+        REQUIRE(leedle.first() == 5);
+        REQUIRE(leedle[0] == 5);
+        REQUIRE(leedle.elementAt(0) == 5);
+    }
+    SECTION("Vector has an end") {
+        REQUIRE(leedle[4] == 45);
+        REQUIRE(leedle.elementAt(4) == 45);
+        REQUIRE(leedle.last() == 45);
+    }
+
 }
